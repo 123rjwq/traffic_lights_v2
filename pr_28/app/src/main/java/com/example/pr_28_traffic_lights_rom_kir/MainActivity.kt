323f6ce455c8f2ca4281efcf4c802aa0e1ba2c2e
123rjwq <145851361+123rjwq@users.noreply.github.com>
2024-04-21 21:02:07 +0300

    1
    начало

---
+++ pr_28/app/src/main/java/com/example/pr_28_traffic_lights_rom_kir/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pr_28_traffic_lights_rom_kir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ pr_28/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "pr_28_traffic_lights_Rom_kir"
+include(":app")
+ 
